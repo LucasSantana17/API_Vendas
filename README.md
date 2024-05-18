de6b9ca964c213c168a255d36c25d32885cc57c7
Lucas <sjoaolucascostasantana@gmail.com>
2024-05-18 12:16:34 -0300

    rotas criadas

---
+++ README.md
@@ -0,0 +1,50 @@
+DESCRIÇÃO DO PROJETO.
+
+1  Criando toda a estrutura básica de funcionamento. 
+
+2  Tentativa de uso do padrão de projeto MVC.
+
+3  Criando rotas para testar o funcionamento.
+
+4  Crud basico criado.
+
+---------
+PROBLEMAS
+
+Resolvidos 
+* Variáves de ambiente erradas, causando erro de conexão com o banco de dados 
+* dados do cpf nulos
+
+PROBLEMAS ATUAIS 
+
+* Tratamento de retorno de infromações 
+
+-------------------------
+
+TRATAMENTOS NECESSÁRIOS
+
+1 TRATAMENTO
+descrição: tratar a rota de (busca por id) para que ela não retorne um array vazio;
+requisitos: entender melhor o fluxo de aplicação e retorno de dados;
+
+--------------------------------------------
+
+IMPLEMENTAÇÕES 
+
+Criar interação entre as tabelas, procurar a melhor forma de fazer isso
+
+Implementar roteamento para o front end 
+
+Refatorar o codigo
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+BOAS PRÁTICA E APRENDIZADO 
+* Para requisições do tipo GET  á melhor forma de acesso é atravez do 
+
+const {id} = req.query;
+
+
+
+
+
--- src/Controller/routes.js
+++ src/Controller/routes.js
@@ -2,24 +2,13 @@ const express = require('express');
 const banco = require('../Model/conexaoBanco');
 const rots = express.Router();
 
+
 rots.get('/teste', (req, rep, next) => {
    return rep.status(200).send({msg:'DEUS é bom, o tempo todo'});
 });
 
-rots.get('/user',(request, response) => {
-   const SQL = 'select * from cliente';
-
-   banco.query(SQL, (err, result) => {
-      if(err){
-         response.status(500).json({msg:'erro ao consultar'});
-      }else{
-         response.status(200).json(result);
-         console.log('Consulta feita com sucesso')
-      }
-   })
-});
-
-rots.post('/cadastro', (req, rep)=>{
+//Criando um cadastro no banco
+rots.post('/cadastro', (req, rep, next)=>{
 
    const sql = 'INSERT INTO cliente (nome, CPF, email) VALUES (?, ?, ?)';
 
@@ -44,6 +33,42 @@ rots.post('/cadastro', (req, rep)=>{
 
 });
 
+// Busca de todos os cadastro no banco
+rots.get('/user',(req, rep, next) => {
+   const SQL = 'select * from cliente';
+
+   banco.query(SQL, (err, result) => {
+      if(err){
+         rep.status(500).json({msg:'erro ao consultar'});
+      }else{
+         rep.status(200).json(result);
+         console.log('Consulta feita com sucesso')
+      }
+   })
+});
+
+// Busca por id 
+rots.get('/busca', (req, res) => {
+
+   const sql = 'SELECT * FROM cliente WHERE id = ?';
+
+   const {id} = req.query;
+   
+   console.log({id});
+
+   banco.query(sql, [id], (err, result) => {
+
+         if(err){
+            res.status(500).send({msg:'Usuário não encontrado'});
+            console.log('Erro ao buscar usuário!'+ err);
+         }else{
+            res.status(200).json(result);
+            console.log('Consulta realizada com sucesso!!');
+         }   
+   });
+});
+
+//Deleção de um registro dentro do banco 
 rots.delete('/delete',(request, response) => {
    const id = request.body.id;
 
@@ -57,5 +82,7 @@ rots.delete('/delete',(request, response) => {
       console.log('deleção feita com sucesso!');
    });
 });
+
+
 
 module.exports = rots;
