c986b2f57d77d5b5a5d7341c6863d3bc1f240da5
Joao Lucas <sjoaolucascostasantana@gmail.com>
2024-06-09 22:34:15 -0300

    Estruturando banco

--- src/Controller/routes.js
+++ src/Controller/routes.js
@@ -9,31 +9,37 @@ rots.get('/teste', (req, rep, next) => {
    return rep.status(200).send({msg:'DEUS é bom, o tempo todo'});
 });
 
+
 //Criando um cadastro no banco
 rots.post('/cadastro', (req, rep, next)=>{
 
    const sql = 'INSERT INTO cliente (nome, CPF, email) VALUES (?, ?, ?)';
 
-   let nome = req.body.nome;
-   let cpf = req.body.CPF;
-   let email = req.body.email;
+   let nome = req.body.nome; console.log(nome)
+   let cpf = req.body.cpf;       console.log(cpf)
+   let email = req.body.email; console.log(email)
 
    if(nome=='' || cpf=='' || email==''){
       rep.status(500).send({msg:'Cadastro invalido, tente novamente!'});
       rep.end();
    }
+   if(cpf == null){
+      rep.status(500).send({msg:'CPF obrigatório'});
+      rep.end();
+   }
 
-   banco.query(sql, [nome, cpf, email], (err, result) => {
+   banco.query(sql, [nome, cpf, email], (err) => {
       if(err){
          console.log('Erro ao cadastrar'+ err);
+         console.log(err)
          rep.status(500).send({msg:'Cadastro não realizado'});
       }else{  
-         rep.status(200).json(result);
+         rep.status(200).json({msg:`Cadastro realizado com sucesso! ${nome, cpf, email}`});
          console.log(`Cadastro realizado com sucesso! ${nome, cpf, email} `)
       }
     });
-
 });
+
 
 // Busca de todos os cadastro no banco
 rots.get('/user',(req, rep, next) => {
@@ -49,14 +55,15 @@ rots.get('/user',(req, rep, next) => {
    })
 });
 
+
 // Busca por id 
 rots.get('/busca', (req, res) => {
    
 
-   const sql = 'SELECT * FROM cliente WHERE id = ?';
+   const sql = 'SELECT * FROM cliente WHERE id_cliente = ?';
 
-   const {id} = req.query;
-   
+   const id = req.body.id;
+
    console.log({id});
 
    banco.query(sql, [id], (err, result) => {
@@ -71,12 +78,29 @@ rots.get('/busca', (req, res) => {
    });
 });
 
+
+rots.get('/buscaNome', (req, rep)=>{
+
+   const sql = 'SELECT * FROM cliente WHERE nome = ?';
+
+   let {nome} = req.body;
+
+    banco.query(sql, [nome], (err, result) => {
+      if(err){
+         rep.status(500).send({msg:'Erro ao procurar'});
+         console.log('erro ao buscar'+ err)
+      }else{
+         rep.status(200).send(result);
+       }
+    });
+})
+
 //Deleção de um registro dentro do banco 
 rots.delete('/delete',(request, response) => {
 
    const id = request.body.id;
 
-   const SQL = 'DELETE FROM cliente WHERE id = ?';
+   const SQL = 'DELETE FROM cliente WHERE id_cliente = ?';
 
    banco.query(SQL, id, (err, result) => {
       if(err){ 
